31fb7b74b50eed36d1113568098b9c53777f9955
RinaOcean <antyfifa@gmail.com>
2021-01-12 12:54:16 +0200

    done

--- js/main.js
+++ js/main.js
@@ -3,13 +3,7 @@ import users from "./users.js";
 // Задание 1
 // Получить массив имен всех пользователей (поле name).
 
-const getUserNames = (users) =>
-  // users.reduce((allNames, user) => {
-  //   allNames.push(user.name);
-
-  //   return allNames;
-  // }, []);
-  users.map((user) => user.name);
+const getUserNames = (users) => users.map((user) => user.name);
 
 console.log(getUserNames(users));
 // [ 'Moore Hensley', 'Sharlene Bush', 'Ross Vazquez', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony' ]
